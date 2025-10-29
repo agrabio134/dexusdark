85252b2612ce59ef7d0d16d6120c33d1c9592a84
agrabio134 <agrabioharvey@gmail.com>
2025-10-29 09:44:48 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const FOURTWENTY = 'CZy3nB9ET6SxBDdAnd7zcaGiPU8JnFQWCwdEZfWhpump';
 const SERIOUSCAT = '8iJhFLFq2SHhZBGKpKK2DfsSaJ62JZRn18dmX3sbpump';
 const PINHEAD = '6mgqeeGHE5GrVk9fYdeJSjKTFZV1TVNAQTMYdHjfpump';
 const ALCH = 'WXsX5HSoVquYRGuJXJrCSogT1M6nZiPRrfZhQsPcXAU';
-
+const POLYMI = 'HygZx5u3aaXg38grfa39Y59NSGFQXSMCUyAYWoZfpump';
 const PLX = '52KWGFoax5Ed1YbFctptXjSShv1P6R3SqUuo6Hk3pump';
 
 const STUPID = 
@@ -648,7 +648,7 @@ function App() {
         const mints = [
           USDARK_CA, XBT, DEEP, JUP_MINT, JTO_MINT,
           PUMP_MINT, USDC_MINT, PANDU, PFP, FSJAL, ALPHA, 
-          TARIFCOIN, LENNY, RAGE, FOURTWENTY, Starecat, SERIOUSCAT, PINHEAD, ALCH, PLX, STUPID, SONIAN
+          TARIFCOIN, LENNY, RAGE, FOURTWENTY, Starecat, SERIOUSCAT, PINHEAD, ALCH, PLX, STUPID, SONIAN, POLYMI
         ];
 
         for (const mint of mints) {
